5eb051ae0417f4081cdbc7a43b6f40f7d02b255e
udaykiran <yuday0555@gmail.com>
2023-12-22 12:12:17 +0530

    terraform

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -7,8 +7,22 @@ terraform {
   }
 }
 
+
+terraform {
+   
+  backend "s3" {
+    bucket = "roboshop-remote-state-1"
+    key = "foreach-demo"
+    region = "us-east-1"
+    dynamodb_table = "roboshop-locking"
+                }
+
+          }
+
+
 provider "aws" {
   # Configuration options
   # you can provide access key and security key here but security problem
   region = "us-east-1"
 }
+
